3a167967533d9925cdf9ab476d152c3f0c1f381f
Elijah Cannon <elijah.cannon@octanner.com>
2023-02-05 01:38:49 -0700

    v1.0

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
-// const CIRCLE_SIZE = 30;
+// const BUBBLE_SIZE = 30;
 p5.disableFriendlyErrors = true; // disables FES to boost performance
-const WIDTH = 800;
-const HEIGHT = 800;
-const center = p5.Vector(WIDTH / 2, HEIGHT / 2);
-const HALF = WIDTH / 2;
+let SCREEN;
+let HALF;
+let BUBBLE_SIZE = 30;
 let score = 0;
 let combo = 0;
 let maxCombo = 0;
@@ -21,19 +20,9 @@ let approachTime;
 let notes = [];
 let activeNotes = [];
 let playedNotes = 0;
-const edgeLength = WIDTH / 2 - CIRCLE_SIZE * 2.5 + 2;
-const cornerLength =
-  Math.sqrt(edgeLength * edgeLength + edgeLength * edgeLength) / 2;
-let bubbles = [
-  { x: HALF, y: HALF - edgeLength, active: false },
-  { x: HALF + cornerLength, y: HALF - cornerLength, active: false },
-  { x: HALF + edgeLength, y: HALF, active: false },
-  { x: HALF + cornerLength, y: HALF + cornerLength, active: false },
-  { x: HALF, y: HALF + edgeLength, active: false },
-  { x: HALF - cornerLength, y: HALF + cornerLength, active: false },
-  { x: HALF - edgeLength, y: HALF, active: false },
-  { x: HALF - cornerLength, y: HALF - cornerLength, active: false },
-];
+let edgeLength;
+let cornerLength;
+let bubbles;
 
 function preload() {
   soundFormats("mp3", "ogg");
@@ -42,17 +31,58 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(WIDTH, HEIGHT);
-  slider = createSlider(0, 1, 0.5, 0.01);
+  const width = windowWidth;
+  const height = windowHeight;
+  SCREEN = width >= height ? height : width;
+  HALF = SCREEN / 2;
+  BUBBLE_SIZE = SCREEN / 30;
+  edgeLength = SCREEN / 2 - 75;
+  cornerLength =
+    Math.sqrt(edgeLength * edgeLength + edgeLength * edgeLength) / 2;
+
+  createCanvas(SCREEN, SCREEN);
   textFont(myFont);
   textSize(24);
+
+  bubbles = [
+    { x: HALF, y: HALF - edgeLength, active: false },
+    { x: HALF + cornerLength, y: HALF - cornerLength, active: false },
+    { x: HALF + edgeLength, y: HALF, active: false },
+    { x: HALF + cornerLength, y: HALF + cornerLength, active: false },
+    { x: HALF, y: HALF + edgeLength, active: false },
+    { x: HALF - cornerLength, y: HALF + cornerLength, active: false },
+    { x: HALF - edgeLength, y: HALF, active: false },
+    { x: HALF - cornerLength, y: HALF - cornerLength, active: false },
+  ];
+}
+
+function windowResized() {
+  const width = windowWidth;
+  const height = windowHeight;
+  SCREEN = width >= height ? height : width;
+  HALF = SCREEN / 2;
+  edgeLength = SCREEN / 2 - 75;
+  cornerLength =
+    Math.sqrt(edgeLength * edgeLength + edgeLength * edgeLength) / 2;
+  resizeCanvas(SCREEN, SCREEN);
+
+  bubbles = [
+    { x: HALF, y: HALF - edgeLength, active: false },
+    { x: HALF + cornerLength, y: HALF - cornerLength, active: false },
+    { x: HALF + edgeLength, y: HALF, active: false },
+    { x: HALF + cornerLength, y: HALF + cornerLength, active: false },
+    { x: HALF, y: HALF + edgeLength, active: false },
+    { x: HALF - cornerLength, y: HALF + cornerLength, active: false },
+    { x: HALF - edgeLength, y: HALF, active: false },
+    { x: HALF - cornerLength, y: HALF - cornerLength, active: false },
+  ];
 }
 
 function start(data) {
   track = data;
   console.log(track);
   notes = track.notes;
-  song.setVolume(slider.value());
+  song.setVolume(0.5);
   song.stop();
   song.play();
   songDuration = song.duration() * 1000;
@@ -68,7 +98,7 @@ function start(data) {
 
 function play() {
   song.play();
-  pauseTime += pauseStartTime ? new Date() - pauseStartTime : 0;
+  pauseTime += pauseStartTime ? new Date() - pauseStartTime : 0; // total time paused
   paused = false;
   console.log("play");
 }
@@ -83,46 +113,49 @@ function pause() {
 function hit() {
   score += 100 + 3 * combo;
   combo++;
-  console.log("Score: " + score);
-  console.log("Combo: " + combo);
+  // console.log("Score: " + score);
+  // console.log("Combo: " + combo);
 }
 
 function miss() {
-  // play crash sound if combo > 25
+  // todo play crash sound if combo > 25
   if (combo > maxCombo) maxCombo = combo;
   combo = 0;
-  console.log("miss");
-}
-
-function complete() {
-  console.log("stage complete");
+  // console.log("miss");
 }
 
 function displayResults(p5) {
+  // todo play sound
   background(0);
+  fill(255);
+  stroke(0);
   textAlign(CENTER);
   textSize(56);
-  text("Score: " + nfc(score), WIDTH / 2, HEIGHT / 2 - 100);
+  text("Score: " + nfc(score), SCREEN / 2, SCREEN / 2 - 100);
   textSize(48);
-  text("Max Combo: " + nfc(maxCombo), WIDTH / 2, HEIGHT / 2);
+  text("Max Combo: " + nfc(maxCombo), SCREEN / 2, SCREEN / 2);
 }
 
 function keyPressed() {
   // console.log(keyCode);
   if (keyCode === 32) {
     if (notesFinished) {
+      console.log("hello world");
       songEnded = true;
       song.stop();
     } else paused ? play() : pause();
+    return false; // prevent scroll
   }
   if (keyCode === 13) {
     if (notesFinished) {
+      console.log("hello world");
       songEnded = true;
       song.stop();
     } else paused ? play() : pause();
   }
   if (keyCode === 27) {
     if (notesFinished) {
+      console.log("hello world");
       songEnded = true;
       song.stop();
     } else paused ? play() : pause();
@@ -130,7 +163,7 @@ function keyPressed() {
 }
 
 const closestBubble = () => {
-  let shortestLength = WIDTH;
+  let shortestLength = SCREEN;
   let shortestBubble;
   bubbles.forEach((b, i) => {
     const d = dist(b.x, b.y, mouseX, mouseY);
@@ -143,42 +176,61 @@ const closestBubble = () => {
 };
 
 function draw() {
-  if (notesFinished & songEnded) {
+  if (notesFinished && songEnded) {
     clear();
     displayResults(p5);
   } else {
-    if (paused) return;
-    playTime = new Date() - startTime - pauseTime;
+    background(0);
+    // text
+    let fps = frameRate();
+    fill(255);
+    stroke(0);
+    textAlign(LEFT);
+    textSize(SCREEN / 15);
+    text(nfc(combo), SCREEN / 80, SCREEN - SCREEN / 80);
+    textAlign(RIGHT);
+    textSize(SCREEN / 28);
+    text(nfc(score), SCREEN - SCREEN / 80, SCREEN / 28);
+    textSize(SCREEN / 66);
+    text("FPS: " + fps.toFixed(2), SCREEN - SCREEN / 80, SCREEN - SCREEN / 80);
+
+    let timePaused = pauseTime;
+    if (paused) {
+      timePaused += new Date() - pauseStartTime;
+    }
+    playTime = new Date() - startTime - timePaused;
+    // line
+    stroke(255, 0, 0);
+    strokeWeight(10);
+    x = map(playTime, 0, songDuration, 0, SCREEN);
+    line(0, 0, x, 0);
+
     noStroke();
     strokeWeight(0);
-    background(0);
-    // rotate(TWO_PI / 16);
-    // translate(WIDTH / 2, HEIGHT / 2);
-    // fill(255);
-    // polygon(0, 0, WIDTH / 2 - CIRCLE_SIZE, 8);
-    // fill(0);
-    // polygon(0, 0, WIDTH / 2 - CIRCLE_SIZE * 2 - 2, 8);
-    // translate(-WIDTH / 2, -HEIGHT / 2);
-    // rotate(-TWO_PI / 16);
 
     // collission bubbles - draw bubbles
     bubbles.map((b, i) => {
-      if (i === closestBubble()) {
-        b.active = true;
-        // fill(143, 203, 155); // make it green
-        fill(0, 255, 51);
-      } else {
-        b.active = false;
-        // fill(255, 255, 200); // yellow
+      if (paused) {
         fill(255);
+      } else {
+        if (i === closestBubble()) {
+          b.active = true;
+          // fill(143, 203, 155); // make it green
+          fill(0, 255, 51);
+        } else {
+          b.active = false;
+          // fill(255, 255, 200); // yellow
+          fill(255);
+        }
       }
-      return ellipse(b.x, b.y, CIRCLE_SIZE);
+      return ellipse(b.x, b.y, BUBBLE_SIZE);
     });
 
+    // if (paused) return;
     fill(239, 35, 60);
 
     try {
-      if (notes.length === 0 && activeNotes.length === 0) {
+      if (notes.length === 0 && activeNotes.length === 0 && playedNotes > 0) {
         notesFinished = true;
         return;
       }
@@ -192,25 +244,27 @@ function draw() {
       }
       // console.log(activeNotes);
       if (activeNotes.length > 0) {
-        activeNotes.forEach((note) => {
+        activeNotes.forEach((note, i) => {
           const life = (note.time - playTime) / approachTime; // (.52) percent of life the note has left;
           const distance = edgeLength - Math.floor(edgeLength * life); // where the note should be based on the timestamp
           note.xPos = distance * note.xPath + HALF;
           note.yPos = distance * note.yPath + HALF;
-          ellipse(note.xPos, note.yPos, CIRCLE_SIZE);
+          ellipse(note.xPos, note.yPos, BUBBLE_SIZE);
           //collision check
           bubbles.forEach((b) => {
             const d = dist(note.xPos, note.yPos, b.x, b.y);
-            if (d < CIRCLE_SIZE && !note.played && b.active) {
+            if (d < BUBBLE_SIZE / 2 && !note.played && b.active) {
               hit();
               note.played = true;
+              activeNotes.splice(i, 1);
             }
           });
+          if (note.played) return;
 
           //exit check
           if (
-            Math.abs(note.xPos) > WIDTH / 2 + HALF ||
-            Math.abs(note.yPos) > WIDTH / 2 + HALF
+            Math.abs(note.xPos) > SCREEN / 2 + HALF ||
+            Math.abs(note.yPos) > SCREEN / 2 + HALF
           ) {
             activeNotes.shift();
             if (!note.played) {
@@ -224,41 +278,8 @@ function draw() {
       pause();
     }
 
-    let fps = frameRate();
-    fill(255);
-    stroke(0);
-    textAlign(LEFT);
-    textSize(56);
-    text(nfc(combo), 10, HEIGHT - 10);
-    textAlign(RIGHT);
-    textSize(28);
-    text(nfc(score), WIDTH - 10, 28);
-    textSize(12);
-    text("FPS: " + fps.toFixed(2), WIDTH - 10, HEIGHT - 10);
-
+    // BLUE ANCHOR CIRCLE
     fill(35, 116, 171);
-    ellipse(HALF, HALF, CIRCLE_SIZE);
-    stroke(255, 0, 0);
-    strokeWeight(10);
-    x = map(playTime, 0, songDuration, 0, WIDTH);
-    line(0, 0, x, 0);
-    // let locX = mouseX - width / 2;
-    // let locY = mouseY - height / 2;
-    // ambientLight(60, 60, 60);
-    // pointLight(35, 116, 171, locX, locY, 50);
-    // specularMaterial(250);
-    // shininess(0);
-    // sphere(30);
+    ellipse(HALF, HALF, BUBBLE_SIZE);
   }
-}
-
-function polygon(x, y, radius, npoints) {
-  let angle = TWO_PI / npoints;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
 }
